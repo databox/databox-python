352bf33e9f3cf468fd4068a2e47aaae250a9ab21
Oto Brglez <otobrglez@gmail.com>
2015-06-03 11:37:13 +0200

    Small changes to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import databox
-import setuptools
+from setuptools import setup, find_packages
 
 install_requires = []
 install_requires.append('requests >= 2.7')
 
-setuptools.setup(
+setup(
     name=databox.__package_name__,
     version=databox.__version__,
     author=databox.__author__,
@@ -12,6 +12,6 @@ setuptools.setup(
     description=databox.__description__,
     url=databox.__url__,
     license='MIT',
-    packages=setuptools.find_packages(exclude=('tests',)),
+    packages=find_packages(exclude=('tests',)),
     install_requires=install_requires
 )
