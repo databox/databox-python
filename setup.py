742d836bac2a2203ec3ea627ab3d3ede5052f512
Oto Brglez <otobrglez@gmail.com>
2015-06-04 15:09:16 +0200

    Changing author adding version 0.1.1

--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="databox",
-    version="0.2.0",
-    author="Vlada Petrović",
+    version="0.1.2",
+    author="Databox",
     author_email="support@databox.com",
     description="Push metrics to Databox.",
     url="https://github.com/databox/databox-python",
     license='MIT',
-
     packages=find_packages(exclude=('databox test',)),
     install_requires=[
         'requests >= 2.7'
