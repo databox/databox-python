1f7a97dbd1954b049032ef7243c97845c371d89c
Slavko Rihtaric <slavko@databox.com>
2024-07-10 11:30:45 +0200

    Cleanup

--- databox/__version__.py
+++
@@ -1,7 +0,0 @@
-__title__ = 'databox'
-__version__ = '2.1.4'
-__description__ = 'Python wrapper for Databox - Mobile Executive Dashboard.'
-__url__ = 'https://github.com/databox/databox-python'
-__author__ = 'Databox'
-__author_email__ = 'support@databox.com'
-__license__ = 'MIT'
